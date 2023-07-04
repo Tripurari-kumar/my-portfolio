344505e231e7a64f359647fb66afefdd75c8c686
Tripurari kumar <tripurari.kumar@lennar.com>
2023-07-05 00:35:34 +0530

    navbar changes done

---
+++ my-portfolio/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Navbar from '@/sections/navbar';
+import React from 'react'
+
+function Index() {
+  return (
+    <div className='app'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Index;
---
+++ my-portfolio/src/sections/navbar.tsx
@@ -0,0 +1,53 @@
+import Button from '@/components/button';
+import Logo from '@/components/logo';
+import Link from 'next/link';
+import React from 'react';
+
+function Navbar() {
+  const sectionLinks = [
+    {
+      name: 'About',
+      link: '/#about',
+    },
+    {
+      name: 'Experience',
+      link: '/#experience',
+    },
+    {
+      name: 'Work',
+      link: '/#work',
+    },
+    {
+      name: 'Contact',
+      link: '/#contact',
+    },
+  ];
+
+  return (
+    <nav>
+      <div className='wrapper'>
+        <div className='brand'>
+          <Link href={'https://github.com/Tripurari-kumar'}>
+            <Logo />
+          </Link>
+        </div>
+        <div className='nav-items'>
+          <ul className='nav-items-list'>
+            {sectionLinks.map(({ name, link }) => (
+              <li key={name} className='nav-items-list-item'>
+                <Link href={link} className='nav-items-list-items-link'>
+                  {name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div className='nav-items-button'>
+            <Button text={'Resume'} link={'http://localhost/3000/resume.pdf'} />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
